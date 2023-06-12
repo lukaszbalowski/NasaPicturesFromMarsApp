9f4af355f5d4552a28b5bef00e4f3fbba647f174
lukaszbalowski <l.balowski@gmail.com>
2023-06-12 20:26:20 +0200

    Initial commit

---
+++ src/main/java/MarsPicturesApp/MarsPicturesAppApplication.java
@@ -0,0 +1,13 @@
+package MarsPicturesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsPicturesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsPicturesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MarsPicturesApp/MarsPicturesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MarsPicturesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsPicturesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
